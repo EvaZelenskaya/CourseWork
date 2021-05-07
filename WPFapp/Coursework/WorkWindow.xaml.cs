4c71328fe6c110c24a8faa654ff67fae264de499
Evka <SuperLuck@ya.ru>
2021-05-07 14:56:01 +0300

    Func_1 - very bad

--- WPFapp/Coursework/WorkWindow.xaml.cs
+++ WPFapp/Coursework/WorkWindow.xaml.cs
@@ -16,6 +16,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Data;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
 
 namespace Coursework
 {
@@ -30,7 +35,57 @@ namespace Coursework
             timer.Tick += timer_Tick;
             timer.Start();
             lblDate.Content = DateTime.Today.ToShortDateString();
+            
+            lbl_buy_Now_currently.Content= GetUSDRate();
+            sum_cash.Text= Msk_Bank();
+
+            
         }
+
+        private String Msk_Bank()
+        {
+            WebClient wc = new WebClient();
+            String Responte = wc.DownloadString(@"https://mkb.ru");
+            String Rate = Regex.Match(Responte, @"<td class='data al_right'>([0-9]+\.[0-9]+)</td>").Groups[1].Value;
+            return Rate;
+        }
+        public static String FindText(string source, string prefix, string suffix)
+        {
+            var prefixPosition = source.IndexOf(prefix, StringComparison.OrdinalIgnoreCase);
+            var suffixPosition = source.IndexOf(suffix, prefixPosition + prefix.Length, StringComparison.OrdinalIgnoreCase);
+
+            if ((prefixPosition >= 0) && (suffixPosition >= 0) && (suffixPosition > prefixPosition) && ((prefixPosition + prefix.Length) <= suffixPosition))
+            {
+                return source.Substring(
+                                prefixPosition + prefix.Length,
+                                suffixPosition - prefixPosition - prefix.Length
+                    );
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+        private String GetUSDRate()
+        {
+            string url = "http://www.cbr.ru/scripts/XML_daily.asp";
+            //XmlDocument xml_doc = new XmlDocument();
+            //xml_doc.Load(url);
+            DataSet ds = new DataSet();
+            ds.ReadXml(url);
+            DataTable currency = ds.Tables["Valute"];
+            foreach (DataRow row in currency.Rows)
+            {
+                if (row["CharCode"].ToString() == "USD")//Ищу нужный код валюты
+                {
+                    return row["Value"].ToString(); //Возвращаю значение курсы валюты
+                }
+            }
+            return "";
+        }
+       
+        
+
         void timer_Tick(object sender, EventArgs e)
         {
             lblTime.Content = DateTime.Now.ToLongTimeString();
